f6f63c864428037de0b656efcef4f18507c76eb5
Pame Ratzam <3550608330101@ingenieria.usac.edu.gt>
2025-08-15 23:38:29 -0600

    hotfix: cambiar canción favorita por álbum favorito

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Versión 2.0: nombre + álbum favorito
 app.get('/', (req, res) => {
   res.json({
-    version: '1.0',
+    version: '2.0',
     nombre: 'Pame Ratzam',
-    cancion_favorita: 'Abba'
+    album_favorito: 'Conxsion'
   });
 });
+
 
 app.listen(PORT, () => {
   console.log(`API corriendo en http://localhost:${PORT}`);
